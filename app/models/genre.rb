2f01284b48e3e3135d3eabfc7b37a4d63dc26115
T0m555 <77041875+T0m555@users.noreply.github.com>
2021-05-24 10:11:01 +0900

    ジャンルをアクティブハッシュからDB実装に変更

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,20 +1,3 @@
-class Genre < ActiveHash::Base
-  self.data = [
-    { id: 1, name: '--' },
-    { id: 2, name: 'Ruby' },
-    { id: 3, name: 'JavaScript' },
-    { id: 4, name: 'HTML, CSS' },
-    { id: 5, name: 'Python' },
-    { id: 6, name: 'C, C++' },
-    { id: 7, name: 'C#' },
-    { id: 8, name: '' },
-    { id: 9, name: '' },
-    { id: 10, name: '' },
-    { id: 11, name: '' },
-    { id: 12, name: '' },
-    { id: 13, name: '' },
-    { id: 14, name: '' }
-  ]
-  include ActiveHash::Associations
+class Genre < ApplicationRecord
   has_many :items
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_04_094503) do
+ActiveRecord::Schema.define(version: 2021_05_24_010402) do
 
   create_table "album_items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "album_id", null: false
@@ -27,6 +27,12 @@ ActiveRecord::Schema.define(version: 2021_05_04_094503) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_albums_on_user_id"
+  end
+
+  create_table "genres", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
